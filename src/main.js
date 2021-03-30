e0a5f1a7bc42cd499e30dc4a9ca9cb831853e6aa
Daniel Williams <dlw1ll27@gmail.com>
2021-03-30 14:19:01 -0700

    adds todays hw - working signup, login, and logout pages

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ Vue.config.productionTip = false;
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:3000" : "/";
 
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
+
 new Vue({
   router,
   render: (h) => h(App),
