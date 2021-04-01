59d15bdc450213b79b159d75ad448561550c9fa1
Daniel Williams <dlw1ll27@gmail.com>
2021-04-01 15:28:02 -0700

    adds bootstrap, cards, and navbar

--- src/App.vue
+++ src/App.vue
@@ -15,29 +15,53 @@
       |
       <router-link to="/movies">Create A Movie</router-link>
     </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Moviezz</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              <router-link to="/" class="nav-link">
+                Home
+                <span class="sr-only">(current)</span>
+              </router-link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">All Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Create Movie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" tabindex="-1">Sign Up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" tabindex="-1">Log In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" tabindex="-1">Log Out</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: "Trebuchet", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  background-image: url("./assets/double-bubble-outline.png");
 }
 </style>
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="movies-index">
-    <div>
+    <div class="jumbotron">
+      <h1 class="display-4">Movie World</h1>
+      <p class="lead">Only the best movies for you my friend.</p>
+      <hr class="my-4" />
+      <p>See a new world of like maybe 7 movies, all in one website. Woah</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-6" v-for="movie in movies" v-bind:key="movie.id">
+        <div class="card">
+          <div class="card-body">
+            <img class="card-img-top" v-bind:src="movie.image_url" v-bind:alt="movie.title" max-width="200px" />
+            <h5 class="card-title">{{ movie.title }}</h5>
+            <p class="card-text">{{ movie.plot }}</p>
+            <router-link v-bind:to="`movies/${movie.id}`">
+              <button type="button" class="btn btn-link">More Info</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div>
       <div v-for="movie in movies" v-bind:key="movie.id">
         <h3>{{ movie.title }} - {{ movie.year }}</h3>
         <p>{{ movie.plot }}</p>
         <router-link v-bind:to="`/movies/${movie.id}`">More Details</router-link>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
-
+<style>
+#card-img-top {
+  width: 100%;
+  height: 15vw;
+  object-fit: cover;
+}
+</style>
 <script>
 import axios from "axios";
 
